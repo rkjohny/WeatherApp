806e2c3649a945dc310fa5918a7eaf67c770bf2b
rkjohny <rkjohny@gmail.com>
2024-07-07 06:57:02 +0600

    moved database credentials in db.properties file

---
+++ src/main/java/com/proit/weatherapp/security/Authority.java
@@ -0,0 +1,10 @@
+package com.proit.weatherapp.security;
+
+public class Authority {
+
+    private Authority() {
+    }
+
+    private static final String USER = "ROLE_USER";
+    private static final String ADMIN = "ROLE_ADMIN";
+}
