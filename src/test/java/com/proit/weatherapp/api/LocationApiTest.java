b7e45023d90ceb4256fef1ce712e7ac7687c22d6
rkjohny <rkjohny@gmail.com>
2024-07-16 07:10:46 +0600

    added LocationApiTest

---
+++ src/test/java/com/proit/weatherapp/api/LocationApiTest.java
@@ -0,0 +1,31 @@
+package com.proit.weatherapp.api;
+
+import com.proit.weatherapp.dto.Location;
+import com.proit.weatherapp.types.GetLocationInput;
+import com.proit.weatherapp.types.GetLocationOutput;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+public class LocationApiTest {
+    @Autowired
+    private LocationApi locationApi;
+
+    @Test
+    public void getLocation() {
+        GetLocationInput input = new GetLocationInput();
+        input.setCity("Dhaka");
+        input.setFilter("Dhaka");
+
+        GetLocationOutput output = locationApi.getLocations(input);
+        List<Location> locations = output.getLocations();
+        assertFalse(locations.isEmpty());
+        assertEquals("dhaka", locations.get(0).getName().toLowerCase());
+    }
+}
